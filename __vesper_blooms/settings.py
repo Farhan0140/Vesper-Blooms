af1f8365511f25bd303a27401b36a5ae301423a9
Farhan Nadim <farhannadim2023@gmail.com>
2025-09-01 21:13:05 +0600

    Created some apps for this project

--- __vesper_blooms/settings.py
+++ __vesper_blooms/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'djoser',
     'api',
     'users',
+    'flowers',
+    'order',
     'django_filters',
 ]
 
--- __vesper_blooms/urls.py
+++ __vesper_blooms/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
